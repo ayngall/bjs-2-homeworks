c6bb93255cdd2dcfd0640a0d4732690fa8d376c3
Aynur Gallyamov <ainyr60@gmail.com>
2022-09-03 23:25:06 +0500

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,11 +1,22 @@
-// Задание 1
+'use strict'
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum = sum + arr[i];
+  }
+  avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
+  
 }
+
 
 // Задание 2
 function worker(arr) {

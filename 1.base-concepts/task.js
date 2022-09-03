5b2c50e170c8808bf16656f121e75d7c7672de1d
Aynur Gallyamov <ainyr60@gmail.com>
2022-09-03 18:33:23 +0500

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,22 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d;
+  d = b**2-4*a*c;
+  if (d<0) {
+   arr=[];
+  } else if (d===0) {
+    arr[0] = -b/(2*a)
+  }
+  else if (d>0) {
+    let x;
+   x = -b/(2*a)
+    arr[0] = (-b + Math.sqrt(d))/(2*a);
+    arr[1] = (-b - Math.sqrt(d))/(2*a);
+  }
   return arr; // array
 }
+solveEquation(2,2,2)
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;

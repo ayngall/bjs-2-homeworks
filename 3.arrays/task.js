4088ef81be1950d5f7cb50e08c683e03b288fd7e
Aynur Gallyamov <ainyr60@gmail.com>
2022-09-10 00:39:18 +0500

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  return (arr1.length === arr2.length) && (arr1.every((val,idx) => val === arr2[idx]));
+}
 
-  // Ваш код
-
-  return result; // boolean
+function advancedFilter(arr) {
+  return arr.filter(val => val > 0).filter(val => val % 3 === 0).map(val => val * 10);
 }
 
 function advancedFilter(arr) {
